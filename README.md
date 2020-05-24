e31428afefa10d7ea9d4d89d8044d6a6a1ee7cc7
9laurence <p.laurence@hotmail.com>
2020-05-23 23:38:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# mortes_abril_amais_covid
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+O objetivo do mortes_abril_amais_covid foi de confirmar o número de mortes a mais que aconteceu em abril, comparado aos anos anteriores, e descobrindo o desvio padrão. A análise foi feita para refazer a análise apresentada no artigo jornalístico "https://g1.globo.com/mundo/blog/helio-gurovitz/post/2020/04/28/o-impacto-real-da-covid-19.ghtml". Diferente do artigo original, também buscou-se descobrir o desvio padrão.
+
---
+++ data-raw/medias_dps.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+
+
+# Dados do "http://tabnet.saude.prefeitura.sp.gov.br/cgi/tabcgi.exe?secretarias/saude/TABNET/SIM/obito.def"
+# Carregar dados de 2015 a 2017 (18 e 19 estão faltando)
+lista <- map(paste0("data-raw/", 2015:2017, ".csv"), 
+             read_csv2, 
+             skip = 3,
+             n_max = 12, na = "-")
+
+# Empilhar os dados de 2015 a 2017 num unico df
+da_empilhado <- bind_rows(lista) %>%
+  janitor::clean_names()
+
+# Fazer a soma do que não é acidente e então a media por mês
+da_media_dp <- da_empilhado %>%
+  mutate(tot = rowSums(.[2:59], na.rm = TRUE)) %>%
+  select(1,69) %>%  
+  group_by(m_ea_s_do_d3_bito) %>% 
+  summarize(media = (mean(tot, na.rm = TRUE)), dp = (sd(tot, na.rm = TRUE))) %>% 
+  filter(m_ea_s_do_d3_bito == "Abril")
+
+# Salvar os dados na pasta data
+write_csv(da_media_dp, "data/media_abril15_17.csv")            
